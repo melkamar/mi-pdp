b5b205a44badadd5dffb77cbd98e1359ca0df921
Martin Melka <melkamar@fit.cvut.cz>
2017-05-03 10:22:22 +0200

    Parametrize starting script

--- jobs/start-all.sh
+++ jobs/start-all.sh
@@ -28,29 +28,38 @@
 #         qrun2 20c 1 pdp_serial serial_job.sh
 #         qrun2 12c 4 pdp_long parallel_job.sh
 
-qrun2 12c 1 pdp_long ./g1-serial.sh
-qrun2 12c 1 pdp_long ./g1-n2.sh
-qrun2 12c 1 pdp_long ./g1-n4.sh
-qrun2 12c 2 pdp_long ./g1-n8.sh
-qrun2 12c 4 pdp_long ./g1-n16.sh
-qrun2 12c 6 pdp_long ./g1-n24.sh
-qrun2 12c 4 pdp_long ./g1-n32.sh
-qrun2 12c 6 pdp_long ./g1-n60.sh
 
-qrun2 12c 1 pdp_long ./g2-serial.sh
-qrun2 12c 1 pdp_long ./g2-n2.sh
-qrun2 12c 1 pdp_long ./g2-n4.sh
-qrun2 12c 2 pdp_long ./g2-n8.sh
-qrun2 12c 4 pdp_long ./g2-n16.sh
-qrun2 12c 6 pdp_long ./g2-n24.sh
-qrun2 12c 4 pdp_long ./g2-n32.sh
-qrun2 12c 6 pdp_long ./g2-n60.sh
 
-qrun2 12c 1 pdp_long ./g3-serial.sh
-qrun2 12c 1 pdp_long ./g3-n2.sh
-qrun2 12c 1 pdp_long ./g3-n4.sh
-qrun2 12c 2 pdp_long ./g3-n8.sh
-qrun2 12c 4 pdp_long ./g3-n16.sh
-qrun2 12c 6 pdp_long ./g3-n24.sh
-qrun2 12c 4 pdp_long ./g3-n32.sh
-qrun2 12c 6 pdp_long ./g3-n60.sh
+if [[ -z "$1" ]]; then
+        echo "Enter queue name as a parameter: [pdp_fast,pdp_long,pdp_serial]"
+        exit 1
+fi
+
+queue_type="$1"
+
+qrun2 12c 1 "${queue_type}" ./g1-serial.sh
+qrun2 12c 1 "${queue_type}" ./g1-n2.sh
+qrun2 12c 1 "${queue_type}" ./g1-n4.sh
+qrun2 12c 2 "${queue_type}" ./g1-n8.sh
+qrun2 12c 4 "${queue_type}" ./g1-n16.sh
+qrun2 12c 6 "${queue_type}" ./g1-n24.sh
+qrun2 12c 4 "${queue_type}" ./g1-n32.sh
+qrun2 12c 6 "${queue_type}" ./g1-n60.sh
+
+qrun2 12c 1 "${queue_type}" ./g2-serial.sh
+qrun2 12c 1 "${queue_type}" ./g2-n2.sh
+qrun2 12c 1 "${queue_type}" ./g2-n4.sh
+qrun2 12c 2 "${queue_type}" ./g2-n8.sh
+qrun2 12c 4 "${queue_type}" ./g2-n16.sh
+qrun2 12c 6 "${queue_type}" ./g2-n24.sh
+qrun2 12c 4 "${queue_type}" ./g2-n32.sh
+qrun2 12c 6 "${queue_type}" ./g2-n60.sh
+
+qrun2 12c 1 "${queue_type}" ./g3-serial.sh
+qrun2 12c 1 "${queue_type}" ./g3-n2.sh
+qrun2 12c 1 "${queue_type}" ./g3-n4.sh
+qrun2 12c 2 "${queue_type}" ./g3-n8.sh
+qrun2 12c 4 "${queue_type}" ./g3-n16.sh
+qrun2 12c 6 "${queue_type}" ./g3-n24.sh
+qrun2 12c 4 "${queue_type}" ./g3-n32.sh
+qrun2 12c 6 "${queue_type}" ./g3-n60.sh
